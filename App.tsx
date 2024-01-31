3bd4efd141f784708fe5ffb3453d46373e86b043
Oscar Saravia <oscarsaraviard0013@gmail.com>
2024-01-31 09:25:58 -0600

    feat: add new screens and dependencies

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Navigator } from './src/navigator/Navigator';
 
 export const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 };
